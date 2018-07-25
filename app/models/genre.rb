40cd3e90c2d3217b047aac1c800ead1074b70d03
Alexandra Fren <alexandra.fren@gmail.com>
2018-07-25 16:51:37 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,8 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists
+    artist_names = []
+    self.artists.each { |artist| artist_names << artist.name }
+    artist_names
   end
 end
